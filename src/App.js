ca583bb3192db42be79e2759824eea0992db0d03
khe4oyan <khe4oyan@gmail.com>
2025-02-07 18:02:55 +0400

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import 'resetcss_khc';
+import styled, {createGlobalStyle} from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  body {
+    min-height: 100dvh;
+  }
+`;
+
+const AppStyles = styled.div`
+  padding: 10px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      sdf
-    </div>
+    <>
+      <AppStyles>
+        ss
+      </AppStyles>
+      <GlobalStyles />
+    </>
   );
 }
 
