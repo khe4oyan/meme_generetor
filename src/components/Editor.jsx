c333bfe1f68075da215ec24eb12e68ffe8b2dbe1
khc <khe4oyan@gmail.com>
2025-02-08 14:11:31 +0400

    .

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -11,7 +11,7 @@ const ControlPanel = styled.div`
   input:nth-child(1) {
     padding: 0;
   }
-  
+
   input {
     padding: 2px 4px;
   }
@@ -19,27 +19,62 @@ const ControlPanel = styled.div`
 
 export default function Editor() {
   const canvasRef = useRef(null);
+  const [ctx, setCtx] = useState(null);
+  const [topText, setTopText] = useState("");
+  const [bottomText, setBottomText] = useState("");
 
   useEffect(() => {
     // canvas initialization
     if (canvasRef.current) {
-      canvasRef.current.width = 500;
-      canvasRef.current.height = 500;
-      canvasRef.current.style.backgroundColor = '#fff';
+      const cnv = canvasRef.current;
+      cnv.width = 500;
+      cnv.height = 500;
+      cnv.style.backgroundColor = "#fff";
+
+      const ctx = cnv.getContext("2d");
+      setCtx(ctx);
     }
   }, []);
 
   const uploadImage = (event) => {
-    // TODO
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const img = new Image();
+    img.onload = () => {
+      canvasRef.current.width = img.width;
+      canvasRef.current.height = img.height;
+      ctx.drawImage(img, 0, 0);
+    };
+
+    img.src = URL.createObjectURL(file);
   };
 
   return (
     <div>
       <canvas id="canvas" ref={canvasRef} />
+
       <ControlPanel>
-        <input type="file" id="uploadInput" accept="image/*" onChange={uploadImage}/>
-        <input type="text" placeholder="Top text"/>
-        <input type="text" placeholder="Bottom text"/>
+        <input
+          type="file"
+          id="uploadInput"
+          accept="image/*"
+          onChange={uploadImage}
+        />
+        
+        <input
+          type="text"
+          placeholder="Top text"
+          value={topText}
+          onChange={(e) => setTopText(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Bottom text"
+          value={bottomText}
+          onChange={(e) => setBottomText(e.target.value)}
+        />
       </ControlPanel>
     </div>
   );
